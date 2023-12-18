dddf189203be6283d55a83f41821d486ae69d0fb
Samuel Hurst <samuel.hurst2@btinternet.com>
2023-12-18 16:26:44 +0000

    login done, needs testing, password functions and dbfucntions done

--- GroupCW/AzureFunctions/shared_code/DBFunctions.py
+++ GroupCW/AzureFunctions/shared_code/DBFunctions.py
@@ -1,4 +1,32 @@
-# TODO: query
-# TODO: create
-# TODO: delete
-# TODO: upsert
+#System Imports
+import logging
+import os
+#Azure Imports
+from azure.cosmos import CosmosClient
+
+#Set up connections to CosmosDB
+DB = CosmosClient(os.environ['URL'], os.environ['KEY'])
+DBProxy = DB.get_database_client(os.environ['Database'])
+
+def query_items(query, container):
+    logging.info("Query Database")
+    #Run Query, return results as list
+    return list(container.query_items(query=query,enable_cross_partition_query=True))
+
+def create_item(data, container):
+    logging.info("Insert Data into Database")
+    #Run insert
+    container.create_item(data, enable_automatic_id_generation = True)
+    return
+
+def upsert_item(data, container):
+    logging.info("Update Data in Database")
+    #Run update
+    container.upsert_item(data)
+    return
+
+def delete_item(id, container):
+    logging.info("Delete Data from Database")
+    #Run delete
+    container.delete_item(item=id, partition_key=id)
+    return
--- GroupCW/AzureFunctions/shared_code/PasswordFunctions.py
+++ GroupCW/AzureFunctions/shared_code/PasswordFunctions.py
@@ -1,2 +1,14 @@
+#System Imports
+import bcrypt
+
 # TODO: Password hashing
 # TODO: Salt generation
+
+def hash_password(password):
+    # Generate salt to hash password with
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hash(password, salt)
+    return hash_password
+
+def verify(passwordToVerify, hashedPassword):
+    return bcrypt.verify(passwordToVerify, hashedPassword)
