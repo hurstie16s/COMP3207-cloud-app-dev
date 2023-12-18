76bc18080b3db213d6edb8ea5afdd368cb9209b0
Samuel Hurst <samuel.hurst2@btinternet.com>
2023-12-18 15:29:51 +0000

    work on login backend functions

--- GroupCW/AzureFunctions/login/__init__.py
+++ GroupCW/AzureFunctions/login/__init__.py
@@ -1,7 +1,17 @@
 # System Imports
+import logging
 # Azure Imports
 from azure.functions import HttpRequest, HttpResponse # TODO: sort issue with import
 #Code base imports
 
 def main(req: HttpRequest) -> HttpResponse:
-    pass
+    
+    logging.info('Python HTTP trigger function processed a request.')
+
+    #Get data from JSON doc
+    input = req.get_json()
+    username = input.get("username")
+    password = input.get("password")
+
+    # Check username exists, grab hashed password based off username
+    
