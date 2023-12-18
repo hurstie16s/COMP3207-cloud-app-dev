4908cb9d87f3cd0e5e02e3beddd98da04792fed6
WillW7 <william.wright03@gmail.com>
2023-12-18 16:37:17 +0000

    Created Vue app

--- GroupCW/app.js
+++ GroupCW/app.js
@@ -1,3 +1,40 @@
 /*
 Vue BackEnd, if possible since we don't need person to person communication do everything on Vue
 */
+
+'use strict';
+
+//Set up express
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+// URL of the backend API
+const BACKEND_ENDPOINT = process.env.BACKEND || 'http://localhost:8181';
+
+//Start the server
+function startServer() {
+    const PORT = process.env.PORT || 8080;
+    server.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`);
+    });
+}
+
+//Setup static page handling
+app.set('view engine', 'ejs');
+app.use('/static', express.static('public'));
+
+//Handle client interface on /
+app.get('/', (req, res) => {
+  res.render('root');
+});
+
+
+
+//Start server
+if (module === require.main) {
+    startServer();
+  }
+  
+module.exports = server;
+  
--- GroupCW/public/VueApp.js
+++ GroupCW/public/VueApp.js
@@ -1,20 +1,25 @@
 var app = new Vue({
-    el: '#app',
+    el: '#VueApp',
     //All data here
     data: {
-        
+      page: 'sign-in',
+      user: null
     },
+    /*
     //On Awake methods here:
     mounted: function() {
        
     },
+    */
     //Js Methods here:
     methods: {
-        
+        nav(page) {
+          app.page = page;
+        }
     },
     //FrontEnd methods here:
     computed: {
-       
+        
       }
 });
 
