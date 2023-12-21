db5a44e495da30162da442160f9593e49dbd545d
WillW7 <william.wright03@gmail.com>
2023-12-21 18:39:57 +0000

    login with dummy users map

--- GroupCW/public/VueApp.js
+++ GroupCW/public/VueApp.js
@@ -3,7 +3,10 @@ var app = new Vue({
     //All data here
     data: {
       page: 'sign-in',
-      user: null
+      user: null,
+
+      username: '',
+      password: ''
     },
     /*
     //On Awake methods here:
@@ -20,6 +23,24 @@ var app = new Vue({
         logout() {
           app.user = null;
           this.nav('sign-in');
+        },
+
+        login() {
+          if (this.username === "") { //empty username
+            //username error
+          } else if (this.password === "") { //empty password
+            //password error
+          } else {
+            const response = handleSignIn(this.username, this.password);
+            if (response.status === 'OK') {
+              app.user = this.username;
+              this.nav('explore');
+            } else {
+              //reponse.msg error
+            }
+          }
+          app.username = '';
+          app.password = '';
         }
     },
     //FrontEnd methods here:
@@ -28,4 +49,24 @@ var app = new Vue({
       }
 });
 
+
 //any functions outside of vue here:
+
+
+//---------------------------------------------------------
+// Dummies
+let userMap = new Map();
+userMap.set('user1', 'password');
+userMap.set('user2', 'password');
+userMap.set('user3', 'password');
+
+function handleSignIn(username, password) {
+  if (userMap.has(username)) {
+    if (password === userMap.get(username));
+    return {status: 'OK', msg: ''};
+  } else {
+    return {status: 'error', msg: 'error msg'};
+  }
+}
+
+
