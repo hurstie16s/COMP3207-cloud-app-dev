73f9dd65a1f1934bd83a0bf5e77fd9a3614baaa4
Samuel Hurst <samuel.hurst2@btinternet.com>
2023-12-23 10:40:18 +0000

    added tests

--- GroupCW/AzureFunctions/tests/test_login.py
+++ GroupCW/AzureFunctions/tests/test_login.py
@@ -1,3 +1,58 @@
 #System Imports
+import unittest
+import requests
+import uuid
 #Azure Imports
-#Code 
+#Code base Imports
+from AzureData import AzureData
+from shared_code import DBFunctions
+
+class TestAddUserFunction(unittest.TestCase):
+    TEST_URL = "http://localhost:7071/login"
+
+    def setUp(self):
+        # Create Dummy Credentials
+        self.testUsername = str(uuid.uuid1().hex)
+        self.testEmail = self.testUsername+"@test.co.uk"
+        self.testPassword = self.testUsername
+
+        data = {
+            "username": self.testUsername,
+            "email": self.testEmail,
+            "password": self.testPassword
+        }
+
+        DBFunctions.create_item(
+            data=data,
+            container=AzureData.containerUsers
+        )
+    
+    def test_user_not_found(self):
+        
+        # Incorrect Username and Password
+        username = "false User"
+        password = "false Password"
+
+        response = requests.get(url=self.TEST_URL, data={"username": username, "password": password})
+        result = response.status_code == 401 and response.content == {"result": False, "msg": "User not found"}
+        self.assertTrue(result)
+
+    def test_password_incorrect(self):
+
+        # Incorrect Password
+        password = "false Password"
+
+        response = requests.get(url=self.TEST_URL, data={"username": self.testUsername, "password": password})
+        result = response.status_code == 401 and response.content == {"result": False, "msg": "Invalid Login"}
+        self.assertTrue(result)
+
+    def test_login_success(self):
+
+        response = requests.get(url=self.TEST_URL, data={"username": self.testUsername, "password": self.testPassword})
+        self.assertEqual(response.status_code, 200)
+
+    def tearDown(self):
+        DBFunctions.delete_item(
+            id=self.testUsername, 
+            container=AzureData.containerUsers
+        )
