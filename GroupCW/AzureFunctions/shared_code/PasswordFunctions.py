971cb9bd96485afba7e0836c07e72c14a72c9e3d
Samuel Hurst <samuel.hurst2@btinternet.com>
2023-12-24 10:39:22 +0000

    type enforcement

--- GroupCW/AzureFunctions/shared_code/PasswordFunctions.py
+++ GroupCW/AzureFunctions/shared_code/PasswordFunctions.py
@@ -2,14 +2,11 @@
 import bcrypt
 import base64
 
-# TODO: Password hashing
-# TODO: Salt generation
-
-def hash_password(password):
+def hash_password(password: str) -> str:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return base64.b64encode(hashed_password).decode('utf-8')
 
-def verify(passwordToVerify, hashedPassword):
+def verify(passwordToVerify: str, hashedPassword: str) -> bool:
     decoded_hashed_password = base64.b64decode(hashedPassword)
     return bcrypt.checkpw(passwordToVerify.encode('utf-8'), decoded_hashed_password)
