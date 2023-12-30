a0ba98b64051a50224ee5bbc9ce442f790e29101
Apishan Thayananthan <45246539+apishant22@users.noreply.github.com>
2023-12-31 00:38:57 +0100

    Fixed silly error in rate_interview and paramatirsed rate_comments (#59)
    * Fixed silly error in rate_interview and paramatirsed rate_comments
    
    * removed GetData

--- GroupCW/AzureFunctions/rate_interview/__init__.py
+++ GroupCW/AzureFunctions/rate_interview/__init__.py
@@ -2,8 +2,9 @@ import logging
 import json
 import uuid
 from azure.functions import HttpRequest, HttpResponse
-import AzureData
 from shared_code import DBFunctions
+import AzureData
+
 
 def main(req: HttpRequest) -> HttpResponse:
     logging.info('rate_interview function processing a request.')
@@ -18,13 +19,23 @@ def main(req: HttpRequest) -> HttpResponse:
         if rating < 1 or rating > 5 or not isinstance(rating, int):
             return HttpResponse(json.dumps({"result": False, "msg": "Rating must be a whole number between 1 and 5"}), status_code=400, mimetype="application/json")
 
-        interview_data = None
-        for item in AzureData.containerInterviewData.query_items(
-            query="SELECT * FROM c WHERE c.id = @id",
-            parameters=[{"name": "@id", "value": interview_id}],
-            enable_cross_partition_query=True):
-            interview_data = item
-            break
+        interview_data_query = "SELECT * FROM c WHERE c.id = @id"
+
+        # Define the parameters
+        parameters = [
+            {"name": "@id", "value": interview_id}
+        ]
+
+        # Use DBFunctions to query the data
+        interview_data_list = DBFunctions.query_items(
+            query=interview_data_query,
+            parameters=parameters,
+            container=AzureData.containerInterviewData
+        )
+
+        # Retrieve the first item from the query results, if any
+        interview_data = interview_data_list[0] if interview_data_list else None
+
 
         if not interview_data:
             return HttpResponse(json.dumps({"result": False, "msg": "Interview data not found for the provided ID"}), status_code=400, mimetype="application/json")
@@ -42,8 +53,7 @@ def main(req: HttpRequest) -> HttpResponse:
         average_rating = round(total_ratings / len(interview_data['ratings']), 1)
 
         # Update the interview data in the database
-        container = AzureData.containerInterviewData
-        average_rating = DBFunctions.rate_interview(interview_id, username, rating, container)
+        AzureData.containerInterviewData.upsert_item(interview_data)
         return HttpResponse(json.dumps({"result": True, "msg": "Rating added successfully", "average_rating": average_rating}), status_code=200, mimetype="application/json")
 
     except ValueError:
