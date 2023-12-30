06602398e37302e89e86b8f901a525f2ebd81c33
Apishan Thayananthan <apishan2013@gmail.com>
2023-12-30 01:31:51 +0100

    Added rate_interview

---
+++ GroupCW/AzureFunctions/rate_interview/__init__.py
@@ -0,0 +1,51 @@
+import logging
+import json
+import uuid
+from azure.functions import HttpRequest, HttpResponse
+import AzureData
+
+def main(req: HttpRequest) -> HttpResponse:
+    logging.info('rate_interview function processing a request.')
+
+    try:
+        req_body = req.get_json()
+        interview_id = req_body.get('id')
+        username = req_body.get('username')
+        rating = req_body.get('rating')
+
+        # Validate the rating
+        if rating < 1 or rating > 5 or not isinstance(rating, int):
+            return HttpResponse(json.dumps({"result": False, "msg": "Rating must be a whole number between 1 and 5"}), status_code=400, mimetype="application/json")
+
+        interview_data = None
+        for item in AzureData.containerInterviewData.query_items(
+            query="SELECT * FROM c WHERE c.id = @id",
+            parameters=[{"name": "@id", "value": interview_id}],
+            enable_cross_partition_query=True):
+            interview_data = item
+            break
+
+        if not interview_data:
+            return HttpResponse(json.dumps({"result": False, "msg": "Interview data not found for the provided ID"}), status_code=400, mimetype="application/json")
+
+        # Check if user has already rated
+        if any(r['username'] == username for r in interview_data.get('ratings', [])):
+            return HttpResponse(json.dumps({"result": False, "msg": "You have already rated this interview"}), status_code=400, mimetype="application/json")
+
+        # Add the rating
+        new_rating = {"username": username, "rating": rating}
+        interview_data.setdefault('ratings', []).append(new_rating)
+
+        # Calculate the new average rating
+        total_ratings = sum(r['rating'] for r in interview_data['ratings'])
+        average_rating = round(total_ratings / len(interview_data['ratings']), 1)
+
+        # Update the interview data in the database
+        AzureData.containerInterviewData.upsert_item(interview_data)
+        return HttpResponse(json.dumps({"result": True, "msg": "Rating added successfully", "average_rating": average_rating}), status_code=200, mimetype="application/json")
+
+    except ValueError:
+        return HttpResponse(json.dumps({"result": False, "msg": "Invalid request body"}), status_code=400, mimetype="application/json")
+    except Exception as e:
+        logging.error(f"Error occurred: {str(e)}")
+        return HttpResponse(json.dumps({"result": False, "msg": f"Failed to rate interview: {str(e)}"}), status_code=500, mimetype="application/json")
