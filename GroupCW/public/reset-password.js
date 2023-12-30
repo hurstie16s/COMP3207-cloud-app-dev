97ae7fdeb0b081858f9ea186ab17416687a0a0bb
Apishan Thayananthan <apishan2013@gmail.com>
2023-12-30 01:32:22 +0100

    Added rate_interview

---
+++ GroupCW/public/reset-password.js
@@ -0,0 +1,63 @@
+var app = new Vue({
+    el: '#reset-password',
+    //All data here
+    data: {
+      user: null,
+
+      email: '',
+      emailError: ''
+      
+    },
+    //On Awake methods here:
+    mounted: function() {
+
+    },
+    //Js Methods here:
+    methods: {
+        logout() {
+          document.cookie = 'user=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/'; //date is the past so browser removes it
+          app.user = null; //change to cookie
+          window.location.href = '/';
+        },
+
+        resetPassword() {
+          app.emailError = '';
+
+          if (app.email) {
+            const response = handlePasswordReset();
+            if (response.result === true) {
+              //TODO:
+              //feedback that password has been reset
+            } else {
+              app.emailError = response.msg;
+            }
+          } else {app.emailError = 'Email Required';}
+        }
+
+    },
+    //FrontEnd methods here:
+    computed: {
+        
+      }
+});
+
+
+
+//---------------------------------------------------------
+// Dummies
+function handlePasswordReset() {
+  //call api
+  return {result: false, msg: 'Email does not exist'}
+}
+
+function getHelper(data, endpoint) {
+
+}
+
+function postHelper(data, endpoint) {
+
+}
+
+function putHelper(data, endpoint) {
+
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+# COMP3207-cloud-app-dev
+
+Doc: https://docs.google.com/document/d/1yVnwa8kj9aXzCfbb7thvh3iec4dCnRDZ05kLQzOwXIE/edit
+
+
+BackEnd put everything in AzureFunctions (everything will be tested locally)
+FronEnd: public/game.js, views/client.ejs, app.js
+Css: public/main.css
+
+!WARNING!
+Do not change the documents name, this is how it is supposed to be called for submission:
+Example Documentation can be found in the doc.
+
+Group Courework Documentation Link: https://www.overleaf.com/9943653534vjkzsrdmnctx#512417
+
+
+Current Teams:
+Backend:
+Sam
+Alyssa
+Apishan
+Lele
+
+FrontEnd:
+Josh
+Ryan
+Will
+
+Css
+- Josh
+
+Writing Documentation:
+Alyssa
+Lele
+
+Proof Reader:
+- Josh
