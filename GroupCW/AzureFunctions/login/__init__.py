1ea42baa7bce8ab7b28e69ab72ad576a4f4c572a
Samuel Hurst <samuel.hurst2@btinternet.com>
2023-12-30 13:04:13 +0000

    checks for type in login

--- GroupCW/AzureFunctions/login/__init__.py
+++ GroupCW/AzureFunctions/login/__init__.py
@@ -65,6 +65,7 @@ def main(req: HttpRequest) -> HttpResponse:
         
     else:
         # Verify Password
+        logging.info("CHECK MESSAGE:: hashed password type: "+str(type(result[0].get("password"))))
         verified = PasswordFunctions.verify(password, result[0].get("password"))
 
         if (verified):
