55de6081aaf0dfaece16b5d422a23e792f1d8e91
Moonzyyy <47296443+Moonzyyy@users.noreply.github.com>
2024-01-02 14:30:52 +0000

    Fixed Query error and made quick test for it to check if query is correct

--- GroupCW/AzureFunctions/interview_search/__init__.py
+++ GroupCW/AzureFunctions/interview_search/__init__.py
@@ -1,25 +1,30 @@
 import AzureData as AzureData
 from azure.functions import HttpRequest, HttpResponse
 import json
+import logging
 
 def main(req: HttpRequest) -> HttpResponse:
     
-    
-    JsonInput = req.get_json()
-    username = JsonInput['username']
-    interviewQuestion = JsonInput['interviewQuestion']
-    query = ""
-    if(interviewQuestion != ""): 
-        query += "where interviewData.interviewQuestion = " + interviewQuestion
-    
-    if(username != ""):
-        if(query == ""):
-            query += "where interviewData.username = " + username
-        else:
-            query += " AND interviewData.username = " + username
-    
-    allInterviews = list(AzureData.containerInterviewData.query_items(query="SELECT * from interviewData " + query, enable_cross_partition_query=True))
-    return HttpResponse(body=json.dumps(allInterviews),mimetype="application/json")
+    try:
+        JsonInput = req.get_json()
+        username = JsonInput.get('username')
+        interviewQuestion = JsonInput.get('interviewQuestion')
+        searchByVariables = ""
+        if(interviewQuestion != ""): 
+            searchByVariables += "where interviewData.interviewQuestion = " + "'" + interviewQuestion + "'"
+        
+        if(username != ""):
+            if(searchByVariables == ""):
+                searchByVariables += "where interviewData.username = " + "'" + username + "'"
+            else:
+                searchByVariables += " AND interviewData.username = " + "'" + username + "'"
+        
+        query = "SELECT * from interviewData " + searchByVariables
+        logging.info("query to be passed: " + query)
+        allInterviews = list(AzureData.containerInterviewData.query_items(query=query, enable_cross_partition_query=True))
+        return HttpResponse(body=json.dumps(allInterviews),mimetype="application/json")
+    except Exception as e:
+        logging.warning(e)
     
 if __name__ == '__main__': 
     main('test')
