19c14df593e487233e7e217dab794ffdd7e54f77
Josh Douglas <josh.douglas0108@gmail.com>
2024-01-06 13:21:09 +0000

    Fixed reset password

--- GroupCW/public/reset-password.js
+++ GroupCW/public/reset-password.js
@@ -1,34 +1,41 @@
 var app = new Vue({
-    el: '#reset-password',
-    //All data here
-    data: {
-      user: null,
+  el: '#reset-password',
+  //All data here
+  data: {
+    user: null,
 
-      email: '',
-      emailError: ''
-      
-    },
-    //On Awake methods here:
-    mounted: function() {
+    email: '',
+    emailError: '',
+    submitted: false,
+    sending: false
+  },
+  //On Awake methods here:
+  mounted: function () {
 
-    },
-    //Js Methods here:
-    methods: {
-        async resetPassword() {
-          app.emailError = '';
-
-          if (app.email) {
-            const res = await axios.put(`${BACKEND_URL}/password/reset`, {email: this.email});
-            if (res.status > 299) {
-              addNotification(`An error occurred: ${res.status} ` + (res.data ? ` ${res.data.msg}` : ''));
-              return;
-            }
-          } else {this.emailError = 'Email Required';}
+  },
+  //Js Methods here:
+  methods: {
+    async resetPassword() {
+      app.emailError = '';
+      this.sending = true;
+      if (app.email) {
+        const res = await axios.put(`${BACKEND_URL}/password/reset`, { email: this.email });
+        if (res.status > 299) {
+          addNotification(`An error occurred: ${res.status} ` + (res.data ? ` ${res.data.msg}` : ''));
+          return;
+        } else {
+          this.submitted = true;
+          this.sending = false;
         }
-
-    },
-    //FrontEnd methods here:
-    computed: {
-        
+      } else {
+        this.emailError = 'Email Required';
+        this.sending = false;
       }
+    }
+
+  },
+  //FrontEnd methods here:
+  computed: {
+
+  }
 });
